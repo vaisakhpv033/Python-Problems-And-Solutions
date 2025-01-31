b9fc1ffa13d0e77f71f1d51fc9557426fcc4522e
Vaisakh P V <vaisakhpv033@gmail.com>
2025-01-31 11:48:09 +0530

    completed second problem

---
+++ Strings/02_given_string_is_palindrome.py
@@ -0,0 +1,25 @@
+def is_palindrome(word):
+    start = 0
+    end = len(word) - 1
+    while start < end:
+        if word[start] != word[end]:
+            return False
+        start += 1
+        end -= 1 
+    return True
+
+# alternative way using slicing
+def is_palindrome_2(word):
+    return word == word[::-1]
+
+# alternative way using recursion
+def is_palindrome_3(word):
+    if len(word) <= 1:
+        return True 
+    return word[0] == word[-1] and is_palindrome_3(word[1:len(word)-1])
+
+if __name__ == "__main__":
+    word = "malayalam"
+    print(is_palindrome(word))
+    print(is_palindrome_2(word))
+    print(is_palindrome_3(word))
